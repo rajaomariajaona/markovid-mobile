6cb62bf37005e54048193fd6125db254ece4b11d
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-06-19 22:50:10 +0300

    switch to prod

--- lib/request.dart
+++ lib/request.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class RestRequest {
   static final BaseOptions _options = BaseOptions(
-    baseUrl: "http://10.0.2.2:3000/api/fokontany",
+    baseUrl: "https://markovid.herokuapp.com/api/fokontany",
     connectTimeout: 5000,
     receiveTimeout: 3000,
     contentType: Headers.formUrlEncodedContentType,
